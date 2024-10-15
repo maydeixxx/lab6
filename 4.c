04dfcd219e32441b5c6e76c2dde128a9e30c2d5c
Alex Amaev <139319582+maydeixxx@users.noreply.github.com>
2024-10-15 12:31:03 +0700

    Update 4.c

--- 4.c
+++ 4.c
@@ -2,24 +2,17 @@
 #include <stdlib.h>
 
 int main(){
-    int cardnum[16];
+    int cardnum[16] = {2, 2, 0, 0, 7, 0, 1, 0, 5, 1, 0, 4, 4, 2, 9, 2};
     int chet, nechet, sum; 
     chet = 0; 
     nechet = 0; 
     sum = 0;
-
-    printf("Input card number: ");
-    for(int i = 0; i < 16; i++) {
-        int z;
-        scanf("%d", &z);
-        cardnum[i] = z;
-    }
     
-    for(int i = 1; i < 16; i += 2) {
+    for(int i = 1; i < 17; i += 2) {
         nechet += cardnum[i];
     }
 
-    for(int i = 0; i < 16; i += 2) {
+    for(int i = 0; i < 17; i += 2) {
         int doubled = cardnum[i] * 2;
         if (doubled > 9) {
             nechet += doubled - 9;
